c5dc2564fbfa3651aca54bf29c992ea0ceab289b
Darwin Yang <darwin.l.yang@gmail.com>
2014-08-28 14:33:28 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,11 +34,8 @@ describe "User pages" do
 		       before { click_button submit }
 
 		        it { should have_title('Sign up') }
-		        it { should have_content('error') }
+		        it { should have_error_message('error') }
 		    end
-
-		    
-      .
 		end
 
 		describe "with valid information" do
@@ -57,8 +54,9 @@ describe "User pages" do
 		        before { click_button submit }
 		        let(:user) { User.find_by(email: 'user@example.com') }
 
-		        it { should have_title(user.name) }
-		        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+		        it { should have_link('Sign out') }
+        		it { should have_title(user.name) }
+        		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 		    end
 		end
 	end
